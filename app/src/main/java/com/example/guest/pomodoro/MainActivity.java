d88b8b9511d69bc131d2eea2893e54025922eb58
Matt Rosanio <mmrosani@syr.edu>
2016-04-22 08:52:55 -0700

    Setup MainActivity, add EditTextViews to CreateDeckActivity

---
+++ app/src/main/java/com/example/guest/pomodoro/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.guest.pomodoro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.makeDeckButton) Button mDeckButton;
+    @Bind(R.id.studyButton) Button mStudyButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mDeckButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.makeDeckButton:
+                Intent intent = new Intent(MainActivity.this, CreateDeckActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
