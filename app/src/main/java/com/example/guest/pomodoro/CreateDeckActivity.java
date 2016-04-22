1577081d6ed63cae1b274e722914a9173120b07f
Matt Rosanio <mmrosani@syr.edu>
2016-04-22 09:40:20 -0700

    Add submit new card button and questions listview to CreateDeckActivity and implement functionality to add and display card questions

--- app/src/main/java/com/example/guest/pomodoro/CreateDeckActivity.java
+++ app/src/main/java/com/example/guest/pomodoro/CreateDeckActivity.java
@@ -2,12 +2,57 @@ package com.example.guest.pomodoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class CreateDeckActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateDeckActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.questionEditText) EditText mQuestionEditText;
+    @Bind(R.id.answerEditText) EditText mAnswerEditText;
+    @Bind(R.id.addCardButton) Button mAddCardButton;
+    @Bind(R.id.cardsListView) ListView mCardsListView;
+    ArrayList<String> questions = new ArrayList<String>();
+    ArrayList<String> answers = new ArrayList<String>();
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_deck);
+        ButterKnife.bind(this);
+
+        mAddCardButton.setOnClickListener(this);
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, questions);
+        mCardsListView.setAdapter(adapter);
+
+        mCardsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(CreateDeckActivity.this, i, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addCardButton:
+                String question = mQuestionEditText.getText().toString();
+                String answer = mAnswerEditText.getText().toString();
+                questions.add(question);
+                answers.add(answer);
+                adapter.notifyDataSetChanged();
+        }
     }
 }
