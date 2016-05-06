22ccba7ce0191b33c067fb9fbf2a42904d167041
Matt Rosanio <mmrosani@syr.edu>
2016-05-06 08:56:26 -0700

    Add authentication to login activity, fix minor bugs in previous implementation, and automatically log in when a new user signs up

--- app/src/main/java/com/example/guest/pomodoro/ui/CreateUserActivity.java
+++ app/src/main/java/com/example/guest/pomodoro/ui/CreateUserActivity.java
@@ -1,15 +1,20 @@
 package com.example.guest.pomodoro.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.guest.pomodoro.Constants;
 import com.example.guest.pomodoro.R;
 import com.example.guest.pomodoro.models.User;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
@@ -26,6 +31,8 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
     @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @Bind(R.id.newAccountButton) Button mNewAccountButton;
     private Firebase mFirebaseRef;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,8 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_create_user);
         ButterKnife.bind(this);
         mFirebaseRef = new Firebase(Constants.FIREBASE_ROOT_URL);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
         mNewAccountButton.setOnClickListener(this);
     }
 
@@ -54,6 +63,37 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
             public void onSuccess(Map<String, Object> result) {
                 String uid = result.get("uid").toString();
                 createUserInFirebaseHelper(name, email, uid);
+                mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        if(authData != null) {
+                            String userUid = authData.getUid();
+                            mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                            Intent intent = new Intent(CreateUserActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        switch(firebaseError.getCode()) {
+                            case FirebaseError.INVALID_EMAIL:
+                            case FirebaseError.USER_DOES_NOT_EXIST:
+                                mEmailEditText.setError("Please check that you entered your email correctly");
+                                break;
+                            case FirebaseError.INVALID_PASSWORD:
+                                mEmailEditText.setError(firebaseError.getMessage());
+                                break;
+                            case FirebaseError.NETWORK_ERROR:
+                                showErrorToast("There was a problem with the network connection");
+                                break;
+                            default:
+                                showErrorToast(firebaseError.toString());
+                        }
+                    }
+                });
             }
 
             @Override
@@ -67,5 +107,9 @@ public class CreateUserActivity extends AppCompatActivity implements View.OnClic
         final Firebase userLocation = new Firebase(Constants.FIREBASE_URL_USERS).child(uid);
         User newUser = new User(name, email);
         userLocation.setValue(newUser);
+    }
+
+    private void showErrorToast(String message) {
+        Toast.makeText(CreateUserActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
