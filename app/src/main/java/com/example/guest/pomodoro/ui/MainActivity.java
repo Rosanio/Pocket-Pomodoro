6ae3c85bd2e81cefc3b969834b90693b0064ce43
Matt Rosanio <mmrosani@syr.edu>
2016-05-06 09:07:46 -0700

    Add logout functionality

--- app/src/main/java/com/example/guest/pomodoro/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/pomodoro/ui/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.guest.pomodoro.Constants;
 import com.example.guest.pomodoro.R;
 import com.example.guest.pomodoro.game.GameActivity;
+import com.firebase.client.Firebase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,12 +24,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.makeDeckButton) Button mDeckButton;
     @Bind(R.id.studyButton) Button mStudyButton;
     @Bind(R.id.emailTextView) TextView mEmailLabel;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mFirebaseRef = new Firebase(Constants.FIREBASE_ROOT_URL);
 
         mDeckButton.setOnClickListener(this);
         mStudyButton.setOnClickListener(this);
@@ -50,5 +58,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Pocket Pomodoro Feedback");
                 startActivity(emailIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void logout() {
+        mFirebaseRef.unauth();
+        takeUserToLoginScreen();
+    }
+
+    private void takeUserToLoginScreen() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
