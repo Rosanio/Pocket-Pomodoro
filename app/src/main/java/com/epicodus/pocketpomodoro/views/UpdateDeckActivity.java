aa77fedcd79177f599da7704ba117eaf42e78d2d
Matt Rosanio <mmrosani@syr.edu>
2016-09-24 16:12:36 -0700

    Setup update deck layout

---
+++ app/src/main/java/com/epicodus/pocketpomodoro/views/UpdateDeckActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.pocketpomodoro.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateDeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_deck);
+    }
+}
